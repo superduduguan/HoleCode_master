4835356ecdcfb1a3abd50f37df485f1ab0e88e12
superduduguan <48684560+superduduguan@users.noreply.github.com>
2021-11-15 15:40:41 +0800

    Batch precessing added
    Batch precessing added

--- multi_model_demo.py
+++ multi_model_demo.py
@@ -20,7 +20,21 @@ import pickle
 from tqdm import tqdm
 from shutil import copyfile
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
- 
+maps = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'e', 6: 'f', 7: 'g', 8: 'g', 9: 'h'}
+def grade_mode(list):
+    list_set = set(list)  # 取list的集合，去除重复元素
+    frequency_dict = {}
+    for i in list_set:  # 遍历每一个list的元素，得到该元素何其对应的个数.count(i)
+        frequency_dict[i] = list.count(i)  # 创建dict; new_dict[key]=value
+    grade_mode = []
+    for key, value in frequency_dict.items():  # 遍历dict的key and value。key:value
+        if value == max(frequency_dict.values()):
+            grade_mode.append(key)
+    return grade_mode
+
+def myargmax(lst):
+    return max(range(len(lst)), key=lst.__getitem__)
+
 class PbModel:
     """用于载入模型并维护独立的session
 
@@ -126,7 +140,7 @@ def defect(normal_hole):
     NUM_CLA_MODEL = 5
     cla_models = []
     for i in range(NUM_CLA_MODEL):
-        cla_model_path = os.path.join(cur_dir, 'model/HoleDefect/HoleDefect' + str(i) + '_450_128_False_20210814_070121.pb')
+        cla_model_path = os.path.join(cur_dir, 'model\\HoleDefect\\HoleDefect' + str(i) + '_450_128_False_20210814_070121.pb')
         cla_models.append(ClaPbModel(cla_model_path))
 
 
@@ -150,9 +164,10 @@ if __name__ == '__main__':
     # 获取当前文件所在目录
     cur_path = os.path.abspath(__file__)
     cur_dir = os.path.dirname(cur_path)
-    SAMPLE_DIR = os.path.join(cur_dir, 'example/origin')
+    SAMPLE_DIR = os.path.join(cur_dir, 'example\\origin')
 
-    pb_path = os.path.join(cur_dir, 'model\HolePosition\HolePosition.pb')
+
+    pb_path = os.path.join(cur_dir, 'model\\HolePosition\\HolePosition.pb')
     paths = get_all_path(SAMPLE_DIR)
     
     sess = tf.Session()
@@ -162,6 +177,8 @@ if __name__ == '__main__':
         sess.graph.as_default()
         tf.import_graph_def(graph_def, name='')
 
+    IMG = []
+    PATH = []
     for path in tqdm(paths):
         if path.split('.')[-1] != 'bmp':
             continue
@@ -171,54 +188,63 @@ if __name__ == '__main__':
             sess.run(tf.global_variables_initializer())
             input_x = sess.graph.get_tensor_by_name('input/img_in:0')
             op = sess.graph.get_tensor_by_name('HoleDetection/LocationResult:0')
-            _input = np.expand_dims(img, 0)
-            a = sess.run(op,  feed_dict={input_x: _input})[0]
-            RATIO = 1.05
-            lux = int((a[0] - RATIO * a[2]) * 80)
-            luy = int((a[1] - RATIO * a[2]) * 80)
-            rdx = int((a[0] + RATIO * a[2]) * 80)
-            rdy = int((a[1] + RATIO * a[2]) * 80)
-            luy, lux = max(0, luy), max(0, lux)
-            rdx, rdy = min(rdx, 80), min(rdy, 80)
-            hole_img = img[luy:rdy, lux:rdx, :]
-
-            length = rdx - lux
-            height = rdy - luy
-            minor = length - height
-            if minor > 0:
-                hole_img = cv2.copyMakeBorder(hole_img, minor // 2, minor - minor // 2, 0, 0, cv2.BORDER_REPLICATE)
-            elif minor < 0:
-                hole_img = cv2.copyMakeBorder(hole_img, 0, 0, (-minor // 2), (-minor) - (-minor // 2), cv2.BORDER_REPLICATE)
-
-            alpha = 8
-            beta = 1.3
-            center = np.array([hole_img.shape[0]//2, hole_img.shape[1]//2])
-            for i in range(hole_img.shape[0]):
-                for j in range(hole_img.shape[1]):
-                    for k in range(hole_img.shape[2]):
-                        new_dist = max(0.3 * hole_img.shape[0] + 2, np.linalg.norm(center-np.array([i, j]))) - 0.3 * hole_img.shape[0] - 2
-                        x = hole_img[i][j][k]
-                        hole_img[i][j][k] = hole_img[i][j][k] / (1 + (new_dist / alpha) ** beta)
-                        
-            hole_img = cv2.resize(hole_img, (48, 48))
-            name = path.split('\\')[-1]
-            if height < 25:
-                print(path, ' is too small...Pay attention!')
-                tdir = os.path.join(cur_dir, 'example/toosmall', path.split('\\')[4] + '!' + name)
-                cv2.imwrite(tdir, hole_img * 255)
-                continue
-            
-            destdir = os.path.join(cur_dir, 'example/normalized', path.split('\\')[4] + '!' + name)
-            cv2.imwrite(destdir, hole_img * 255)
+            # _input = np.expand_dims(img, 0)
+            IMG.append(img)
+            PATH.append(path)
         except Exception as e:
             print(path, e)
+            
+    IMG = np.array(IMG)
+
+    A = sess.run(op,  feed_dict={input_x: IMG})
+    RATIO = 1.05
+    index = -1
+    for a in A:
+        index += 1
+        lux = int((a[0] - RATIO * a[2]) * 80)
+        luy = int((a[1] - RATIO * a[2]) * 80)
+        rdx = int((a[0] + RATIO * a[2]) * 80)
+        rdy = int((a[1] + RATIO * a[2]) * 80)
+        luy, lux = max(0, luy), max(0, lux)
+        rdx, rdy = min(rdx, 80), min(rdy, 80)
+        hole_img = IMG[index][luy:rdy, lux:rdx, :]
+
+        length = rdx - lux
+        height = rdy - luy
+        minor = length - height
+        if minor > 0:
+            hole_img = cv2.copyMakeBorder(hole_img, minor // 2, minor - minor // 2, 0, 0, cv2.BORDER_REPLICATE)
+        elif minor < 0:
+            hole_img = cv2.copyMakeBorder(hole_img, 0, 0, (-minor // 2), (-minor) - (-minor // 2), cv2.BORDER_REPLICATE)
+
+        alpha = 8
+        beta = 1.3
+        center = np.array([hole_img.shape[0]//2, hole_img.shape[1]//2])
+        for i in range(hole_img.shape[0]):
+            for j in range(hole_img.shape[1]):
+                for k in range(hole_img.shape[2]):
+                    new_dist = max(0.3 * hole_img.shape[0] + 2, np.linalg.norm(center-np.array([i, j]))) - 0.3 * hole_img.shape[0] - 2
+                    x = hole_img[i][j][k]
+                    hole_img[i][j][k] = hole_img[i][j][k] / (1 + (new_dist / alpha) ** beta)
+                    
+        hole_img = cv2.resize(hole_img, (48, 48))
+        name = PATH[index].split('\\')[-1]
+        if height < 25:
+            print(PATH[index], ' is too small...Pay attention!')
+            tdir = os.path.join(cur_dir, 'example\\toosmall', PATH[index].split('\\')[4] + '!' + name)
+            cv2.imwrite(tdir, hole_img * 255)
+            continue
+        
+        destdir = os.path.join(cur_dir, 'example\\normalized', PATH[index].split('\\')[4] + '!' + name)
+        
+        cv2.imwrite(destdir, hole_img * 255)
+            
     print('normalization finished')
-    normdir = os.path.join(cur_dir, 'example/normalized')
+    normdir = os.path.join(cur_dir, 'example\\normalized')
     all_paths = get_all_path(normdir)
 
     # defect
     ALLSCORE = []
-    GT = []
     PATH = []
     IMG = []
     for path in tqdm(all_paths):
@@ -229,12 +255,28 @@ if __name__ == '__main__':
         img = img.astype('float64')
         normal_hole = Preprocess4Defect(img)
         IMG.append(normal_hole)
-        gt = path.split('\\')[-1].split('!')[-2]
-        GT.append(gt)
+
 
     IMG = np.array(IMG)
+    
     IMG = IMG.squeeze(axis=1)
     ALLSCORE = defect(IMG)
-    scores = list(np.array(ALLSCORE).squeeze(axis=-1).T)
-    ALLSCORE = [list(i) for i in scores]
-    print(ALLSCORE)
+    scores = np.array(ALLSCORE)
+    scores = np.transpose(scores, (1, 0, 2))
+    samples = [list(i) for i in scores]  
+
+    predicts = []
+    for sample in samples:  
+        scores = [list(j) for j in sample]  
+        SC = []
+
+        for score in scores:  
+            SC.append(myargmax(score))
+            score[myargmax(score)] = 0.0
+
+        mode = grade_mode(SC)[-1]
+
+        predicts.append(maps[mode])
+    
+    print('predicts: ', predicts)
+
